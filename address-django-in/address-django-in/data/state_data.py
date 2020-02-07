7d2d21f0a6e7fd10e6e9ab81072f7f316069aa40
chetan_redbytes <chetan@redbytes.in>
2020-02-07 18:20:32 +0530

    data preparation for package

---
+++ address-django-in/address-django-in/data/state_data.py
@@ -0,0 +1,26 @@
+state = {
+    'ANDAMAN_AND_NICOBAR_ISLANDS': {
+        'PinCodeStart': [74],
+    },
+    'ANDHRA_PRADESH': {
+        'PinCodeStart': [51, 52, 53],
+    },
+    'ARUNACHAL_PRADESH': {
+        'PinCodeStart': [79],
+    },
+    'ASSAM': {
+        'PinCodeStart': [78],
+    },
+    'BIHAR': {
+        'PinCodeStart': [80, 81, 82, 84, 85],
+    },
+    'CHANDIGARH': {
+        'PinCodeStart': [16],
+    },
+    'CHATTISGARH': {
+        'PinCodeStart': [49],
+    },
+    'DADAR_AND_NAGAR_HAVELI': {
+        'PinCodeStart': [39],
+    }
+}
